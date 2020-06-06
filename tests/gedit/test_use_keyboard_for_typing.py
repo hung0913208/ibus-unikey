c8d25469c374e4f10dbc2d24b5393ae2211c97de
Hung Nguyen Xuan Pham <hung.nx.pham@dektech.com.au>
2020-06-06 23:19:43 +0700

    [UPGRADE][Gedit] run tests with visual terminals
    Description:
    - I just found an issue relates to perform test-cases on display 0 using
      ssh and X11 Forward. During testing, even if we instructs ibus to
      switch to engin Unikey, the ibus-unikey can't work as expected, it
      might relate to issues `unable to type vietnamese after reboot`.
    - This issue seems can't be reproduce if we interfact directly on
      display so i intend using visual terminals to test first.
    
    Signed-off-by: Hung Nguyen Xuan Pham <hung.nx.pham@dektech.com.au>

--- tests/gedit/test_use_keyboard_for_typing.py
+++ tests/gedit/test_use_keyboard_for_typing.py
@@ -1,8 +1,8 @@
-import unittest, os, signal, sys
+import unittest, os, signal, sys, time
 
 from dogtail import tree
 from dogtail.config import config
-from dogtail.procedural import focus, click, run, FocusWidget, FocusDialog, FocusWindow, FocusApplication
+from dogtail.procedural import focus, click, run, FocusWidget, FocusDialog, FocusWindow, FocusApplication, FocusError
 from dogtail.rawinput import typeText, keyCombo
 from dogtail.utils import screenshot
 from dogtail.predicate import GenericPredicate
@@ -16,66 +16,29 @@ def run_ui_typing_helper(cache, text):
     if os.path.isfile(cache):
         os.remove(cache)
 
-    pid = run('gedit')
+    pid = run('gedit {}'.format(cache))
     gedit = tree.root.application('gedit')
-    saved = False
-    ready_to_save = False
+
+    focus.application('gedit')    
+    focus.text()
+    typeText(text)
+    
+    keyCombo('<Control>s')
 
     try:
-        focus.application('gedit')
-     
-        focus.text()
-        typeText(text)
+        # @NOTE: maybe we can't access the menu and click the 
+        # item `Quit` at menu `File`
+    
+        click.menu('File')
+        click.menuItem('Quit')
+    except Exception as error:
+        keyCombo('<Control>q')
 
-        # Click gedit's Save button.
-        click.button('Save')
-
-        try:
-            # Focus gedit's Save As... dialog
-
-            focus.widget.findByPredicate(GenericPredicate(roleName='file chooser'))
-        finally:
-            ready_to_save = True
-    except FocusError as error:
-        try:
-            # This string changed somewhere around gedit 2.13.2.
-            # This is the new string
-
-            focus.dialog('Save As\u2026')
-        except FocusError:
-            # Fall back to the old string.
-
-            focus.dialog('Save as...')
-        finally:
-            ready_to_save = True
-    finally:
-        try:
-            if ready_to_save:
-                try:
-                    typeText(cache)
-                    click('Save')
-                finally:
-                    saved = True
-
-            focus.application('gedit')
-
-            # Let's quit now.
-            try:
-                # @NOTE: maybe we can't access the menu and click the item `Quit`
-                # at menu `File`
-
-                click.menu('File')
-                click.menuItem('Quit')
-            except Exception as error:
-                keyCombo('<Control>q')
-        except Exception as error:
-            print('can\'t quit app, here is the reason: {}'.format(error))
-            os.kill(pid, signal.SIGKILL)
-
-    if saved:
+    try:
         with open(cache) as fd:
             return fd.read()
-    else:
+    except Exception as error:
+        os.kill(pid, signal.SIGKILL)
         return None
 
 class TestKeyboardTyping(unittest.TestCase):
@@ -85,18 +48,19 @@ class TestKeyboardTyping(unittest.TestCase):
     def test_typing_vietnamese_style(self):
         if method == 'telex':
             steps = [
-                ('xin chaof, tooi laf mootj con autobot đuowcj xaya dungj ddeer tesst ibus-unikey',
-                 'xin chào, tôi là một con autobot được xây dựng để test ibus-unikey\n')
+                ('xin chaof, tooi laf mootj con autobot dduowcj xaay duwngj ddeer tesst ibus',
+                 'xin chào, tôi là một con autobot được xây dựng để test ibus\n')
             ]
         elif method == 'vni':
             steps = [
-                ('xin chao2, to6i la2 mo65t con autobot d9uo75c xa6y du7ng5 d9e63 test ibus-unikey',
-                 'xin chào, tôi là một con autobot được xây dựng để test ibus-unikey\n')
+                ('xin chao2, to6i la2 mo65t con autobot d9uo75c xa6y du7ng5 d9e63 test ibus',
+                 'xin chào, tôi là một con autobot được xây dựng để test ibus\n')
             ]
 
-        for typing, expect in steps:
-            result = run_ui_typing_helper('/tmp/0001.txt', typing)
-            self.assertEqual(result, expect)
+        for _ in range(loop):
+            for typing, expect in steps:
+                self.assertEqual(run_ui_typing_helper('/tmp/0001.txt', typing),
+                                 expect)
 
 if __name__ == '__main__':
     #import dogtail.i18n
@@ -105,6 +69,12 @@ if __name__ == '__main__':
     #config.debugSleep = True
     #config.debugSearching = True
     #config.debugTranslation = True
+    #config.actionDelay = 0.1
+
+    if len(sys.argv) > 2:
+        loop = int(sys.argv.pop())
+    else:
+        loop = 1
 
     if len(sys.argv) > 1:
         method = sys.argv.pop()
